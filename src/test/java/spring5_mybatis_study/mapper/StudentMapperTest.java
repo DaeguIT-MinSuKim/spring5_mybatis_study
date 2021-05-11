361831bf592c269aba069263ad59d5e1a857d06c
mskim <net94@naver.com>
2021-05-11 16:42:57 +0900

    1:1 mapping

--- src/test/java/spring5_mybatis_study/mapper/StudentMapperTest.java
+++ src/test/java/spring5_mybatis_study/mapper/StudentMapperTest.java
@@ -4,6 +4,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
@@ -118,4 +120,27 @@ public class StudentMapperTest {
 		Assert.assertSame(1, deleteStudent);
 	}
 
+	@Test
+	public void test07SelectStudentByAllForHashMap(){
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		List<Map<String, Object>> list = mapper.selectStudentByAllForHashMap();
+		Assert.assertNotNull(list);
+		for (Map<String, Object> map : list) {
+			for (Entry<String, Object> e : map.entrySet()) {
+				log.debug(String.format("%s -> %s", e.getKey(), e.getValue()));
+			}
+		}
+	}
+	
+	@Test
+	public void test10SelectStudentByNoAssociation() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName()+"()");
+		
+		Student student = new Student();
+		student.setStudId(1);
+		Student seletedStd = mapper.selectStudentByIdAssociation(student);
+		Assert.assertNotNull(seletedStd);
+		
+		log.debug(seletedStd.toString());
+	}
 }
